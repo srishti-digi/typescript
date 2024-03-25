f61fe9f8cc3018e70dc4f5db7da3a044f5e3d1a5
srishti-digi <smishra@digisprint.com>
2024-03-25 13:00:51 +0530

    variable ts added

---
+++ variable.js
@@ -0,0 +1,5 @@
+var greetings = "Srishti";
+console.log(greetings);
+var userId = 456778;
+userId.toFixed();
+var isLoggedIn = false;
---
+++ variable.ts
@@ -0,0 +1,7 @@
+let greetings: string="Srishti";
+console.log(greetings);
+
+let userId=456778;
+userId.toFixed()
+
+let isLoggedIn:boolean=false;
